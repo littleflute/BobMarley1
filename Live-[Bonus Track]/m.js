1550988ae7924950aed00d132329bb1f2706516c
littleflute <13699175164@sina.cn>
2017-06-14 14:48:14 -0500

    Add files via upload

---
+++ Live-[Bonus Track]/m.js
@@ -0,0 +1,10 @@
+var f = [];				
+f[0]	= "v0.0.8";			
+f[1]	= "01 Trenchtown Rock.mp3";		
+f[2]	= "02 Burnin' and Lootin'.mp3";		
+f[3]	= "03 Them Belly Full (But We Hungry).mp3";		
+f[4]	= "04 Lively Up Yourself.mp3";		
+f[5]	= "05 No Woman, No Cry.mp3";		
+f[6]	= "06 I Shot the Sheriff.mp3";		
+f[7]	= "07 Get Up, Stand Up.mp3";		
+f[8]	= "08 Kinky Reggae [-].mp3";		
